a34ec3cb62a23997c3bd0d8b749afeaf265f9065
kvv1618 <varun.kesharaju@gmail.com>
2025-04-18 17:12:57 -0400

    print model summary

--- data/climate_etl.py
+++ data/climate_etl.py
@@ -4,14 +4,14 @@ import time
 import json
 import os
 
-lat_range = [30, 60]
-lon_range = [-120, -70]
+lat_range = [10, 34]
+lon_range = [72, 88]
 start_date = "1995-02-02"
 end_date = "2024-02-02"
 columns = ['temperature_2m_max', 'temperature_2m_min', 'sunrise', 'sunset', 'daylight_duration', 'sunshine_duration', 'rain_sum', 'showers_sum', 'snowfall_sum', 'precipitation_sum', 'precipitation_probability_max', 'wind_speed_10m_max', 'apparent_temperature_max', 'apparent_temperature_min', 'precipitation_hours', 'et0_fao_evapotranspiration', 'date']
 global_df = pd.DataFrame(columns=columns)
-for lat in range(lat_range[0], lat_range[1], 10):
-    for lon in range(lon_range[0], lon_range[1], 10):
+for lat in range(lat_range[0], lat_range[1], 4):
+    for lon in range(lon_range[0], lon_range[1], 4):
         base_url = f"https://archive-api.open-meteo.com/v1/archive?latitude={lat}&longitude={lon}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_mean,temperature_2m_min,temperature_2m_max,apparent_temperature_mean,apparent_temperature_max,apparent_temperature_min,sunshine_duration,precipitation_sum,rain_sum,snowfall_sum,precipitation_hours"
         while True:
             data = requests.get(base_url)
